781136a6ae04da8154422f71223ef0578a344e4e
Manuel Bocchia <zerounodos@gmail.com>
2022-08-14 22:17:38 -0300

    Found a way to make bubble sort work with recursive method.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,24 @@
 def bubble_sort(array)
-    ordered_array = []
-    sorting_array = array.each_with_index {|num, ind|
-      unless ind +1 == array.length
-        if num > array[ind+1]
-            newnumb = array[ind+1]
-            array[ind+1] = num
-            array[ind] = newnumb
+    $size = array.length
+    $ordered_array = []
+    def sort_max(small_arr)
+    sorting_array = small_arr.each_with_index {|num, ind|
+      unless ind +1 == small_arr.length
+        if num > small_arr[ind+1]
+            newnumb = small_arr[ind+1]
+            small_arr[ind+1] = num
+            small_arr[ind] = newnumb
         end
     end
     }
-    p sorting_array
+    if $ordered_array.length != $size
+    $ordered_array.unshift(sorting_array[-1])
+    sorting_array.pop
+    sort_max(sorting_array)
+    end
+    $ordered_array
+end
+    p sort_max(array)
 end
 
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([4,5,6,1,2,6,7,1,20,40,50])
